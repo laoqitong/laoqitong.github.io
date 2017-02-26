b9ac42d99bb6a62db1cf974c50082b6078d2c344
lqt <727683549@qq.com>
2017-02-26 15:59:53 +0800

    测试反向代理

--- js/common/common.js
+++ js/common/common.js
@@ -1,5 +1,23 @@
 define(['jquery'], function ($) {
+
+    // 左侧导航下拉列表
     $('.navs a').on('click', function () {
         $(this).next().slideToggle();
+    });
+
+
+    $.ajax({
+        url: '/v6/login',
+        type: 'post',
+        data: {
+            tc_name: 11111,
+            tc_pwd: 1111,
+        },
+        success: function () {
+            console.log('ok!');
+        },
+        error: function () {
+            console.log('failed');
+        }
     })
 })
